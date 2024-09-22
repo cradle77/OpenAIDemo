c63b954165798f45a1d6cc9308c68740e355e13c
MarcoDes <marcodesanctis2@gmail.com>
2024-09-22 23:14:15 +0100

    manual streaming function call

--- OpenAIDemo/Server/Controllers/ChatController.cs
+++ OpenAIDemo/Server/Controllers/ChatController.cs
@@ -57,7 +57,7 @@ namespace OpenAIDemo.Server.Controllers
             {
                 MaxTokens = 500,
                 Temperature = 0.7f,
-                ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+                ToolCallBehavior = ToolCallBehavior.EnableKernelFunctions
             }, _kernel);
 
             string responseMessage = result.ToString();
@@ -84,21 +84,77 @@ namespace OpenAIDemo.Server.Controllers
             {
                 MaxTokens = 500,
                 Temperature = 0.7f,
-                ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+                ToolCallBehavior = ToolCallBehavior.EnableKernelFunctions
             }, _kernel);
 
-            var fullResponse = string.Empty;
-
-            await foreach (var responseMessage in result)
+            while(true)
             {
-                fullResponse += responseMessage.Content;
+                var fullResponse = string.Empty;
 
-                yield return responseMessage.Content;
+#pragma warning disable SKEXP0001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+                var functionCallBuilder = new FunctionCallContentBuilder();
+#pragma warning restore SKEXP0001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
+                await foreach (var responseMessage in result)
+                {
+                    if (responseMessage.Content != null)
+                    {
+                        fullResponse += responseMessage.Content;
+
+                        yield return responseMessage.Content;
+                    }
+                    
+                    functionCallBuilder.Append(responseMessage);
+                }
+
+                history.AddAssistantMessage(fullResponse);
+
+                // handling functions
+                var functionCalls = functionCallBuilder.Build();
+
+                if (!functionCalls.Any())
+                {
+                    break; // no function calls, the loop is finished
+                }
+
+                Console.WriteLine($"Requested execution of {functionCalls.Count()} functions");
+
+                // step 1: add the request container to the history
+                var functionRequests = new ChatMessageContent(
+                    role: AuthorRole.Assistant, 
+                    content: null);
+
+                foreach (var functionRequest in functionCalls)
+                {
+                    functionRequests.Items.Add(functionRequest);
+                }
+                history.Add(functionRequests);
+
+                // Step 2: trigger the execution and await
+                var functionExecutions =
+                    functionCalls.Select(async f => 
+                    {
+                        try
+                        {
+                            return await f.InvokeAsync(_kernel);
+                        }
+                        catch (Exception ex)
+                        {
+#pragma warning disable SKEXP0001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+                            return new FunctionResultContent(f, ex);
+#pragma warning restore SKEXP0001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+                        }
+                        
+                    });
+
+                var functionResponses = await Task.WhenAll(functionExecutions);
+
+                // step 3: add the requests and responses to the history
+                foreach (var functionResponse in functionResponses)
+                {
+                    history.Add(functionResponse.ToChatMessage());
+                }
             }
-
-            history.AddAssistantMessage(fullResponse);
-
-            history.ShowLastLog();
 
             history.ShowCount();
         }
